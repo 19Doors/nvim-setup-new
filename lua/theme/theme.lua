78fad13ac6c2d9f25ef6b14fb4bc1d968a0b62a8
19Doors <sakaarsri1904@gmail.com>
2022-05-24 14:36:39 +0530

    Updated

--- lua/theme/theme.lua
+++ lua/theme/theme.lua
@@ -1,8 +1,5 @@
-vim.cmd([[
-let g:onedark_style = 'darker'
-colorscheme onedark]])
-
--- vim.g.tokyonight_style = "night"
- -- vim.cmd([[
- -- colorscheme gruvbox
- -- ]])
+-- Lua
+require('onedark').setup {
+    style = 'darker'
+}
+require('onedark').load()
